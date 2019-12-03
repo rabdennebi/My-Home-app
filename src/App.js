d6b6c656ce273be33a9f745b06a4ab65e75a4223
rabdennebi <rabahabdennebi@hotmail.fr>
2019-12-04 00:19:59 +0100

    add antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { version, Button } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <h1>antd version: {version}</h1>
+      <p>
+        <Button type="primary">Button</Button>
+        Please <b>fork</b> this sandbox to reproduce your issue.
+      </p>
     </div>
   );
 }
